74b4cef75b5fc6d81c24c6447c77394bafb7d536
Rudra <01fe22bca037@kletech.ac.in>
2024-01-20 12:40:59 +0530

    code for arithmatic opr

---
+++ git.java
@@ -0,0 +1,20 @@
+import java.io.*;
+ 
+  class arithmatic {
+    {
+        int num1 = 10, num2 = 20, sum = 0, sub, multi, divi;
+ 
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        sum = num1 + num2;
+        sub = num1 - num2;
+        multi = num1 * num2;
+        divi = num1/num2;
+        System.out.println("The sum = " + sum);
+        System.out.println("The sub = " + sub);
+        System.out.println("The multi = " + multi);
+        System.out.println("The divi = " + divi);
+        
+        
+    }
+}
